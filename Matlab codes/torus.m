917031c3ef4b861fc80011b4431be4a6a20fb060
Sid <62798224+raghuveer-sid@users.noreply.github.com>
2021-11-12 12:49:39 +0100

    Delete Matlab codes directory

--- Matlab codes/torus.m
+++
@@ -1,9 +0,0 @@
-% ----- Function Subroutine Below ----- %
-function [x,y,z] = torusmod(r,d,p)
-[th,phi] = meshgrid(-3.1:0.1:3.2);
-x = (r*cos(th/p)+d).*cos(phi);
-y = (r*cos(th/p)+d).*sin(phi);
-z = r/2*sin(th/p);
-end
-
-%%
